3b0930d7f92280790934a4a8e30901394c2b9f15
Michal Struna <michal.l.struna@gmail.com>
2023-12-03 12:41:13 +0100

    add day03

---
+++ 2023/day03/index.mjs
@@ -0,0 +1,52 @@
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const content = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8")
+const lines = content.split("\n")
+let sumPartNumbers = 0
+const potentialGears = {}
+
+const checkSurrounding = (lineIndex, index, data) => {
+	const lastIndex = index + data.length - 1
+
+	const surroundingsCoords = [[lineIndex, index - 1], [lineIndex, lastIndex + 1]]
+
+	for (let i = index - 1; i <= lastIndex + 1; i++) {
+		surroundingsCoords.push([lineIndex - 1, i], [lineIndex + 1, i])
+	}
+
+	let isSurrounded = false
+
+	for (const [lineIndex, index] of surroundingsCoords) {
+		const char = lines[lineIndex]?.[index]
+		if (char !== undefined && char !== ".") isSurrounded = true
+		if (char === "*") {
+			const coords = `${lineIndex}:${index}`
+			if (!potentialGears[coords]) potentialGears[coords] = []
+			potentialGears[coords].push(parseInt(data))
+		}
+	}
+
+	return isSurrounded
+
+}
+
+for (let i = 0; i < lines.length; i++) {
+	const line = lines[i]
+	const numberMatches = Array.from(line.matchAll(/[0-9]+/g))
+
+	for (const match of numberMatches) {
+		if (checkSurrounding(i, match.index, match[0])) {
+			sumPartNumbers += parseInt(match[0])
+		}
+	}
+}
+
+const gears = Object.values(potentialGears).filter(gear => gear.length === 2)
+const sumGearRatios = gears.reduce((sum, [a, b]) => sum + a * b, 0)
+
+console.log("Sum of part numbers:", sumPartNumbers)
+console.log("Sum of gear ratios:", sumGearRatios)
